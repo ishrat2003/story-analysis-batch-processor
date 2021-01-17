74cdd454173cc0d23507687884ba18ff4b279336
Ishrat Rahman Sami <ishrat@thebyte9.com>
2021-01-17 15:00:56 +0000

    @wip Gc

--- scripts/process.sh
+++ scripts/process.sh
@@ -7,13 +7,15 @@ backupZipName=$(date +'%Y_%m_%d_%H_%M_%S')
 
 totalItems=0
 
-# zip -r "$archieveDirectory/processed_data/SA_$backupZipName.zip" .
-rm -R $destinationDirectory/words/*
-rm -R $destinationDirectory/gc/*
+zip -r "$archieveDirectory/processed_data/SA_$backupZipName.zip" .
+# rm -R $destinationDirectory/words/*
+# rm -R $destinationDirectory/gc/*
 
 python3 code/scripts/documentProcessor.py --source_directory "$sourceDirectory/content" --destination_directory $destinationDirectory --total_items $totalItems
 
-# cd "$sourceDirectory/content"
-# zip -r "$archieveDirectory/processed_feeds/CONTENT_$backupZipName.zip" .
-# cd "$sourceDirectory/rss"
-# zip -r "$archieveDirectory/processed_feeds/RSS_$backupZipName.zip" .
+cd "$sourceDirectory/content"
+zip -r "$archieveDirectory/processed_feeds/CONTENT_$backupZipName.zip" .
+rm *.json
+cd "$sourceDirectory/rss"
+zip -r "$archieveDirectory/processed_feeds/RSS_$backupZipName.zip" .
+rm *.json
